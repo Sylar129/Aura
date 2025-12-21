25ab9bbcbaa7cc096a4b839b871c43d6cdfc2371
Sylar129 <710288202@qq.com>
2025-12-21 22:14:05 +0800

    Game Mode

---
+++ Source/Aura/Private/Game/AuraGameModeBase.cpp
@@ -0,0 +1,5 @@
+// Copyright Sylar129
+
+
+#include "Game/AuraGameModeBase.h"
+
---
+++ Source/Aura/Public/Game/AuraGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Sylar129
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "AuraGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class AURA_API AAuraGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
