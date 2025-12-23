a02dbb7b7151b3d76cd4a2bb156ba6c0b8578051
Sylar129 <710288202@qq.com>
2025-12-23 20:27:02 +0800

    Aura User Widget and Widget Controller

---
+++ Source/Aura/Private/UI/Widget/AuraUserWidget.cpp
@@ -0,0 +1,9 @@
+// Copyright Sylar129
+
+#include "UI/Widget/AuraUserWidget.h"
+
+void UAuraUserWidget::SetWidgetController(UObject* InWidgetController)
+{
+	WidgetController = InWidgetController;
+	WidgetControllerSet();
+}
---
+++ Source/Aura/Private/UI/WidgetController/AuraWidgetController.cpp
@@ -0,0 +1,3 @@
+// Copyright Sylar129
+
+#include "UI/WidgetController/AuraWidgetController.h"
