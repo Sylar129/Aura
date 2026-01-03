06f9e0082e58d697302a152c6452066f04128916
Sylar129 <710288202@qq.com>
2026-01-03 16:05:24 +0800

    Attribute Menu Widget Controller

---
+++ Source/Aura/Private/UI/WidgetController/AttributeMenuWidgetController.cpp
@@ -0,0 +1,11 @@
+// Copyright Sylar129
+
+#include "UI/WidgetController/AttributeMenuWidgetController.h"
+
+void UAttributeMenuWidgetController::BroadcastInitialValues()
+{
+}
+
+void UAttributeMenuWidgetController::BindCallbacksToDependencies()
+{
+}
